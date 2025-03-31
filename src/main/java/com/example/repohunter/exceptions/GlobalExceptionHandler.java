19c38d69ce8e6d5da23f8e79e9899cea4261162f
przemyslawwlodarczyk90 <przemyslawwlodarczyk90@wp.pl>
2025-03-31 21:45:35 +0200

    added gitignore

--- src/main/java/com/example/repohunter/exceptions/GlobalExceptionHandler.java
+++ src/main/java/com/example/repohunter/exceptions/GlobalExceptionHandler.java
@@ -1,7 +1,7 @@
 package com.example.repohunter.exceptions;
 
 import com.example.repohunter.dto.ErrorResponseDto;
-import com.example.repohunter.exceptions.GitHubUserNotFoundException;
+
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.ExceptionHandler;

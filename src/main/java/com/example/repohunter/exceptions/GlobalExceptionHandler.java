7a29c518788852226112df0cb5708b7ff6605c52
przemyslawwlodarczyk90 <przemyslawwlodarczyk90@wp.pl>
2025-03-31 21:29:36 +0200

    feat: add global exception handler for GitHubUserNotFoundException

---
+++ src/main/java/com/example/repohunter/exceptions/GlobalExceptionHandler.java
@@ -0,0 +1,22 @@
+package com.example.repohunter.exceptions;
+
+import com.example.repohunter.dto.ErrorResponseDto;
+import com.example.repohunter.exceptions.GitHubUserNotFoundException;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.RestControllerAdvice;
+
+
+@RestControllerAdvice
+public class GlobalExceptionHandler {
+
+
+    @ExceptionHandler(GitHubUserNotFoundException.class)
+    public ResponseEntity<ErrorResponseDto> handleUserNotFound(GitHubUserNotFoundException ex) {
+        ErrorResponseDto error = new ErrorResponseDto(HttpStatus.NOT_FOUND.value(), ex.getMessage());
+        return new ResponseEntity<>(error, HttpStatus.NOT_FOUND);
+    }
+
+
+}

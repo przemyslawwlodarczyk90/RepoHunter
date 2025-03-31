cae9edfcc7c23727a822599e6e0b23ed3454a40b
przemyslawwlodarczyk90 <przemyslawwlodarczyk90@wp.pl>
2025-03-31 20:01:25 +0200

    init commit

---
+++ src/test/java/com/example/repohunter/RepoHunterApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.repohunter;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RepoHunterApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
